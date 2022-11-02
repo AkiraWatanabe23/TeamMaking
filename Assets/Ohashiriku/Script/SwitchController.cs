a0a85de68df6e0c3d40725f136646f66ebc4b05e
RK <riku.kuu0421@gmail.com>
2022-11-02 16:00:09 +0900

    SwitchControllerを作成

---
+++ Assets/Ohashiriku/Script/SwitchController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onEvent = default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //タグ指定は仮、あとでインターフェースで実装する
+        if(collision.gameObject.tag == "Player")
+        {
+            _onEvent.Invoke();
+        }
+    }
+}
