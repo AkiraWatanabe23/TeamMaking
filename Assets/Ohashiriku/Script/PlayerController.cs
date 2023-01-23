ffe16923df9411ee886e812d05d0dcc040005e08
RK <riku.kuu0421@gmail.com>
2023-01-23 22:04:33 +0900

    [update]プレイヤーの動きはできた

--- Assets/Ohashiriku/Script/PlayerController.cs
+++ Assets/Ohashiriku/Script/PlayerController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    private Rigidbody2D _rb2D;
+    [SerializeField]
+    private float _moveSpeed = 5f;
     void Start()
     {
-        
+        _rb2D = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
-        
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        _rb2D.velocity = new Vector2(horizontal * _moveSpeed, _rb2D.velocity.y);
+
     }
 }
