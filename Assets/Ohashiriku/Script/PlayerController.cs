9bafb211bf31cfe450cac909713167cd11e14012
RK <riku.kuu0421@gmail.com>
2023-01-25 13:54:21 +0900

    [update]変更

--- Assets/Ohashiriku/Script/PlayerController.cs
+++ Assets/Ohashiriku/Script/PlayerController.cs
@@ -7,14 +7,37 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb2D;
     [SerializeField]
     private float _moveSpeed = 5f;
+    Animator _anim;
     void Start()
     {
         _rb2D = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
     void Update()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         _rb2D.velocity = new Vector2(horizontal * _moveSpeed, _rb2D.velocity.y);
+        if (horizontal > 0)
+        {
+            _anim.Play("PlayerRight");
+        }
+        else if (horizontal < 0)
+        {
+            _anim.Play("PlayerLeft");
+        }
+        else
+        {
+            _anim.Play("TestPlayer");
+        }
+        bool jumpKey = Input.GetButtonDown("Jump");
+        //if (jumpKey)
+        //{
+        //    if (checkGround.GetCheckGround())
+        //    {
+        //        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //ƒWƒƒƒ“ƒv‚ÌŒvŽZ
+        //        _anim.SetBool("isJump", true);
+        //    }
 
+        //}
     }
 }
