fe5248380f960c6117828c1bdbf9db6200584f5b
Akira Watanabe <a0323w@gmail.com>
2023-01-26 23:17:05 +0900

    [add] Player俯瞰Animation

--- Assets/Akira/Scripts/PlayerTest.cs
+++ Assets/Akira/Scripts/PlayerTest.cs
@@ -8,16 +8,34 @@ public class PlayerTest : MonoBehaviour
     [SerializeField] private float _moveSpeed = 1f;
 
     private Rigidbody2D _rb = default;
+    private Animator _anim = default;
+    private SpriteRenderer _renderer = default;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
+
+        //移動の入力があれば
+        if (h != 0 || v != 0)
+        {
+            _anim.SetBool("IsIdle", false);
+            _anim.SetBool("IsWalk", true);
+
+            _renderer.flipX = h > 0 ? false : true;
+        }
+        else
+        {
+            _anim.SetBool("IsIdle", true);
+            _anim.SetBool("IsWalk", false);
+        }
 
         _rb.velocity = new Vector2(h * _moveSpeed, v * _moveSpeed);
     }
