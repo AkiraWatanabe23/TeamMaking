7400c23761782ef9d27d0c341b5dcf1142acbba2
Akira Watanabe <a0323w@gmail.com>
2023-01-29 21:42:02 +0900

    [add] テキストテスト開始

---
+++ Assets/Akira/Scripts/IndicateText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// テキスト表示のテスト
+/// </summary>
+public class IndicateText : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
