14d3d304aad9aab898ee6b70b05c760c48d3bc78
Akira Watanabe <a0323w@gmail.com>
2023-01-30 18:45:28 +0900

    [update] DoTweenによるテキスト表示

--- Assets/Akira/Scripts/IndicateText.cs
+++ Assets/Akira/Scripts/IndicateText.cs
@@ -10,13 +10,13 @@ public class IndicateText : MonoBehaviour
     [Tooltip("テキスト表示する時間")]
     [SerializeField] private float _indicateTime = 1f;
     [Tooltip("表示させるText")]
-    [SerializeField] private Text _text = default;
-    [SerializeField] private string _open = default;
+    [SerializeField] private Text _viewText = default;
+    [SerializeField] private Text _open = default;
 
     private void Start()
     {
         //DOText...指定した文字列を指定した時間で1文字ずつ表示する
-        _text.DOText(_open, _indicateTime).SetEase(Ease.Linear);
+        _viewText.DOText($"{_open.text}", _indicateTime).SetEase(Ease.Linear);
     }
 
     private void Update()
