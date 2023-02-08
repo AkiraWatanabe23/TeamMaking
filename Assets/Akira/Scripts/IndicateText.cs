a473f078ac05606adeb2c0052df3f502605f4eff
Akira Watanabe <a0323w@gmail.com>
2023-02-08 11:22:15 +0900

    [update] テキスト表示テスト中

--- Assets/Akira/Scripts/IndicateText.cs
+++ Assets/Akira/Scripts/IndicateText.cs
@@ -7,20 +7,29 @@ using DG.Tweening;
 /// </summary>
 public class IndicateText : MonoBehaviour
 {
-    [Tooltip("テキスト表示する時間")]
+    [Tooltip("何秒かけて表示させるか")]
     [SerializeField] private float _indicateTime = 1f;
     [Tooltip("表示させるText")]
     [SerializeField] private Text _viewText = default;
     [SerializeField] private Text _open = default;
+    [TextArea(1, 3)]
+    [SerializeField] private string[] _testText = default;
+
+    private int _index = 0;
 
     private void Start()
     {
-        //DOText...指定した文字列を指定した時間で1文字ずつ表示する
-        _viewText.DOText($"{_open.text}", _indicateTime).SetEase(Ease.Linear);
+        TextPlay();
     }
 
-    private void Update()
+    public void TextPlay()
     {
-        
+        _viewText.text = "";
+        //DOText...指定した文字列を指定した時間で1文字ずつ表示する
+        _viewText.DOText(_testText[_index], _indicateTime).SetEase(Ease.Linear);
+        if (_index == _testText.Length - 1)
+            _index = 0;
+        else
+            _index++;
     }
 }
