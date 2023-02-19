ff8842228a035392885c1f4aa1f898a84bf6ab6b
Akira Watanabe <a0323w@gmail.com>
2023-02-19 21:56:41 +0900

    [add] 設定パネル(キーボード選択可)

---
+++ Assets/Akira/Scripts/SettingUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    [SerializeField] private Image _settingPanel = default;
+    [SerializeField] private Selectable _startElement = default;
+
+    private Selectable _currentElement = default;
+
+    private void Start()
+    {
+        if (_startElement != null)
+        {
+            _currentElement = _startElement;
+            _currentElement.Select();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_settingPanel != null)
+                _settingPanel.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_currentElement != null)
+                _currentElement.OnDeselect(null);
+
+            if (Input.GetKey(KeyCode.LeftShift) ||
+                Input.GetKey(KeyCode.RightShift))
+                _currentElement = _currentElement.FindSelectableOnUp();
+            else
+                _currentElement = _currentElement.FindSelectableOnDown();
+
+            if (_currentElement != null)
+                _currentElement.Select();
+        }
+    }
+}
